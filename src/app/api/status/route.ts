47ec9fce765d19b820a1b8113dacc1ae4a466940
palmix <10962888+palmix@users.noreply.github.com>
2024-06-04 21:07:01 +0300

    testsc

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,10 +1,19 @@
 import dayjs from 'dayjs';
 import { NextResponse } from 'next/server';
 import abbreviate from 'number-abbreviate';
-import firebase_app  from '../../../../lib/firebase/firebase';
 import { getDatabase, ref, child, get } from 'firebase/database';
 
-
+import { initializeApp} from 'firebase/app';
+const firebaseConfig = {
+    apiKey: 'AIzaSyCw3JVh02EDc7oUkmXv8BXGRF2y05SmmJg',
+    authDomain: 'palestine-2f5c9.firebaseapp.com',
+    projectId: 'palestine-2f5c9',
+    storageBucket: 'palestine-2f5c9.appspot.com',
+    messagingSenderId: '424929570831',
+    appId: '1:424929570831:web:b3c63f40f9063af955b37a',
+    measurementId: 'G-RBT1BWRF6F'
+};
+let firebase_app = initializeApp(firebaseConfig);
 
 export const runtime = 'edge';
 /*
